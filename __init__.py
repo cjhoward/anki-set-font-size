233f9aa528b6cf46b7c6f4171ce88a7d75debc0c
C. J. Howard <cjhoward@cjhoward.org>
2025-05-01 18:31:21 -0700

    Update for compatibility with Anki 24.06.3+.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2018  Christopher James Howard
+# Copyright (C) 2025 C. J. Howard
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -34,8 +34,7 @@ def changeWebFontSize(font_size):
 		# Qt5
 		wes = QWebEngineSettings.globalSettings()
 	
-	#wes.setFontSize(QWebEngineSettings.DefaultFontSize, font_size)
-	wes.setFontSize(QWebEngineSettings.MinimumFontSize, font_size)
+	wes.setFontSize(QWebEngineSettings.FontSize.MinimumFontSize, font_size)
 
 def changeFontSize(config):
     font_size = config['font_size']
